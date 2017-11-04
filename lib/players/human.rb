3f0adff3af1ff14d6ebbb1c58ef72b2b14b9b9f0
Monica  Karasek <mlkarasek@gmail.com>
2017-11-04 21:45:11 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,11 +1,11 @@
 module Players
 
-  class Human < Player 
+  class Human < Player
 
-    def move(position) 
-      puts "Please enter a valid move: 1-9"
+    def move(position)
+      puts "Please enter a valid move."
       position.gets.strip
       position
-    end 
+    end
 
-  end 
+  end
