6f9d38aac5accc2362d30e9b0e9ae634a9d2a833
Monica  Karasek <mlkarasek@gmail.com>
2017-11-04 20:59:49 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -3,20 +3,20 @@ class Board
   attr_accessor :cells
 
   def initialize
-    @board = Board.new 
+    @board = Board.new
     self.reset!
   end
 
-  def reset 
+  def reset
     @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", ]
-  end 
+  end
 
-    def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
 
 end
