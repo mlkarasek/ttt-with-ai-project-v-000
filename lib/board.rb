9986af0c5f0e5351d5441efbb3a487ee4b882e7d
Monica  Karasek <mlkarasek@gmail.com>
2017-11-04 20:46:35 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,14 @@
 class Board
+
+  attr_accessor :cells 
+  attr_reader :game, :player 
+
+  def initialize(cells)
+    @cells = :cells 
+  end 
+
+  def self.reset 
+      all.clear
+    end
+    
 end 
